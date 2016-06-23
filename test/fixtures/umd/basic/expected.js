36ab74c53470b59cde62ec3715968f93793dd358
Thodoris Greasidis <thgreasi@gmail.com>
2016-06-23 08:59:56 +0300

    test: add basic relative import test

--- test/fixtures/umd/basic/expected.js
+++ test/fixtures/umd/basic/expected.js
@@ -9,3 +9,11 @@ typeof _systemImportTransformerGlobalIdentifier.define === 'function' && _system
     typeof module !== 'undefined' && module.exports && typeof require !== 'undefined' || typeof module !== 'undefined' && module.component && _systemImportTransformerGlobalIdentifier.require && _systemImportTransformerGlobalIdentifier.require.loader === 'component' ?
     Promise.resolve(require(('npmModule'))) :
     Promise.resolve(_systemImportTransformerGlobalIdentifier['npmModule']);
+
+typeof _systemImportTransformerGlobalIdentifier.define === 'function' && _systemImportTransformerGlobalIdentifier.define.amd ?
+    new Promise(function (resolve, reject) {
+        _systemImportTransformerGlobalIdentifier.require(['./myModule'], resolve, reject);
+    }) :
+    typeof module !== 'undefined' && module.exports && typeof require !== 'undefined' || typeof module !== 'undefined' && module.component && _systemImportTransformerGlobalIdentifier.require && _systemImportTransformerGlobalIdentifier.require.loader === 'component' ?
+    Promise.resolve(require(('./myModule'))) :
+    Promise.resolve(_systemImportTransformerGlobalIdentifier['./myModule']);
